4a727cb5a8a6180cdd4f689ee9f323b54da61f88
CJ-Cate <168867196+CJ-Cate@users.noreply.github.com>
2025-12-28 20:23:58 -0700

    Added

---
+++ src/main/java/io/github/cj_cate/ideationitems/Items/Backend/InteractEffectClasses/InteractEffect_EntityDeathEvent.java
@@ -0,0 +1,22 @@
+package io.github.cj_cate.ideationitems.Items.Backend.InteractEffectClasses;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent; //
+
+import java.util.function.Consumer;
+
+public class InteractEffect_EntityDeathEvent extends InteractEffectHolder implements Listener //
+{
+
+    private final Consumer<EntityDeathEvent> eventImplementation; //
+
+    public InteractEffect_EntityDeathEvent(Consumer<EntityDeathEvent> eventImplementation) { //
+        this.eventImplementation = eventImplementation;
+    }
+
+    @EventHandler
+    public void eventToRegister(EntityDeathEvent event) { //
+        eventImplementation.accept(event);
+    }
+}
