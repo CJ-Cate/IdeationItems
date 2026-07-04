796bf29ff4272b08d32d18548b9a4543289b0061
CJ-Cate <168867196+CJ-Cate@users.noreply.github.com>
2026-07-03 18:10:25 -0600

    added listener notation

--- src/main/java/io/github/cj_cate/ideationitems/Events/SmithingTableEvents.java
+++ src/main/java/io/github/cj_cate/ideationitems/Events/SmithingTableEvents.java
@@ -2,10 +2,11 @@ package io.github.cj_cate.ideationitems.Events;
 
 import io.github.cj_cate.ideationitems.Utils.TagUtil;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareSmithingEvent;
 import org.bukkit.inventory.ItemStack;
 
-public class SmithingTableEvents {
+public class SmithingTableEvents implements Listener {
     /**
      * If there is a vanilla item CATEGORY output, then change it with the true vanilla item.
      */
